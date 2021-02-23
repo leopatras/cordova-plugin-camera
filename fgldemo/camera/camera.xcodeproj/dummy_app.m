e963f8c3cdc404d4a0a07362477d47c8c50d5881
Leo Schubert <ls@4js.com>
2021-02-23 14:18:29 +0100

    update xcode proj

--- fgldemo/camera/camera.xcodeproj/dummy_app.m
+++ fgldemo/camera/camera.xcodeproj/dummy_app.m
@@ -1,5 +1,25 @@
-/* we provide a dummy main here */
+/* we provide a dummy main here and some more Cordova Stubs */
 int main(int argc, char *argv[]) 
 {
     return 0;
+}
+@interface CDVPlugin:NSObject
+{
+}
+@end
+@implementation CDVPlugin
+{
+}
+@end
+@interface CDVPluginResult:NSObject
+{
+}
+@end
+@implementation CDVPluginResult
+{
+}
+@end
+BOOL IsAtLeastiOSVersion(NSString* ver)
+{
+  return TRUE;
 }
